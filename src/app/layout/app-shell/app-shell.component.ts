c4f1564268bf6f9a63fa306ce924abe59a26e303
7iomka <7iomka@gmail.com>
2026-03-31 17:08:44 +0300

    fix(chat): restore chat and keep footer in shell flow

--- src/app/layout/app-shell/app-shell.component.ts
+++ src/app/layout/app-shell/app-shell.component.ts
@@ -1,8 +1,5 @@
-import { Component, DestroyRef, inject } from '@angular/core';
-import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
-
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { filter } from 'rxjs';
+import { Component, inject } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 import { SidebarModeService } from '../../core/theme/sidebar-mode.service';
 import { AppFooterComponent } from '../components/app-footer.component';
@@ -38,9 +35,7 @@ import { AppSidebarComponent } from '../components/app-sidebar.component';
             <router-outlet />
           </main>
 
-          @if (showFooter) {
-            <app-footer class="app-shell__footer" />
-          }
+          <app-footer class="app-shell__footer" />
         </div>
       </div>
 
@@ -51,23 +46,4 @@ import { AppSidebarComponent } from '../components/app-sidebar.component';
 })
 export class AppShellComponent {
   protected readonly sidebarModeService = inject(SidebarModeService);
-  private readonly router = inject(Router);
-  private readonly destroyRef = inject(DestroyRef);
-
-  protected showFooter = true;
-
-  constructor() {
-    this.syncFooterVisibility(this.router.url);
-
-    this.router.events
-      .pipe(
-        filter((event): event is NavigationEnd => event instanceof NavigationEnd),
-        takeUntilDestroyed(this.destroyRef),
-      )
-      .subscribe((event) => this.syncFooterVisibility(event.urlAfterRedirects));
-  }
-
-  private syncFooterVisibility(url: string): void {
-    this.showFooter = !url.startsWith('/chat');
-  }
 }
